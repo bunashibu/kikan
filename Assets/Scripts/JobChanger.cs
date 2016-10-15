8b43380b2bee5e5551e880ff350560fdf2b36def
siun <siuncyclone@gmail.com>
2016-10-15 14:15:25 +0900

    Add JobChanger

---
+++ Assets/Scripts/JobChanger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JobChanger : MonoBehaviour {
+  public void pickManji() {
+
+  }
+
+  public void pickMagician() {
+
+  }
+
+  public void pickWarrior() {
+
+  }
+}
