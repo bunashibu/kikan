2a3daf90d8f20d68909d0c69bf23528302e1c34e
siun <siuncyclone@gmail.com>
2016-10-15 13:07:42 +0900

    Animation: Login button

--- Assets/Scripts/Login.cs
+++ Assets/Scripts/Login.cs
@@ -2,14 +2,7 @@
 using System.Collections;
 
 public class Login : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+  public void toLobby() {
+    Application.LoadLevel("lobby");
+  }
 }
