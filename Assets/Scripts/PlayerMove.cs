650562a167eee65f0d567a925dffc19b32dca553
siun <siuncyclone@gmail.com>
2016-10-19 21:16:46 +0900

    Move player right and left

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMove : MonoBehaviour {
+  public void MoveRight() {
+    if (Input.GetKey(KeyCode.RightArrow)) {
+      gameObject.transform.position += Vector3.right * 0.02f;
+      GetComponent<SpriteRenderer>().flipX = true;
+    }
+  }
+
+  public void MoveLeft() {
+    if (Input.GetKey(KeyCode.LeftArrow)) {
+      gameObject.transform.position += Vector3.left * 0.02f;
+      GetComponent<SpriteRenderer>().flipX = false;
+    }
+  }
+
+  public void Move() {
+    MoveRight();
+    MoveLeft();
+  }
+
+  void Update() {
+    Move();
+  }
+}
