090bbfa1657f21d97ef8fafdc1e123d7b3befd99
siun <siuncyclone@gmail.com>
2016-10-21 00:28:50 +0900

    Impl Skill

--- Assets/Scripts/Job.cs
+++ Assets/Scripts/Job.cs
@@ -1,7 +1,13 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Job : MonoBehaviour {
+  protected IEnumerator DelayMethod(float waitTime, Action action) {
+    yield return new WaitForSeconds(waitTime);
+    action();
+  }
+
   [SerializeField] protected Sprite _actionNormal;
   [SerializeField] protected Sprite _actionX;
   [SerializeField] protected Sprite _actionShift;
--- Assets/Scripts/Magician.cs
+++ Assets/Scripts/Magician.cs
@@ -1,8 +1,31 @@
 ﻿using UnityEngine;
+using System;
 using System.Collections;
 
 public class Magician : Job {
+  public void SkillX() {
+    GetComponent<SpriteRenderer>().sprite = _actionX;
+    StartCoroutine(DelayMethod(0.2f, () => {
+      GetComponent<SpriteRenderer>().sprite = _actionNormal;
+    }));
+  }
+
+  public void SkillShift() {
+    GetComponent<SpriteRenderer>().sprite = _actionShift;
+    StartCoroutine(DelayMethod(0.5f, () => {
+      GetComponent<SpriteRenderer>().sprite = _actionNormal;
+    }));
+  }
+
   void Start() {
     GetComponent<SpriteRenderer>().sprite = _actionNormal;
+  }
+
+  void Update() {
+    if (Input.GetKey(KeyCode.X))
+      SkillX();
+
+    if (Input.GetKey(KeyCode.LeftShift))
+      SkillShift();
   }
 }
