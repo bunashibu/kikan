b175d438668b178e214c6499166b2f0ae0d01314
siun <siuncyclone@gmail.com>
2016-10-29 15:23:13 +0900

    Add TimeLimit

---
+++ Assets/Scripts/TimeLimit.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class TimeLimit : MonoBehaviour {
+  void Start() {
+    GetComponent<Text>().text = ((int)time).ToString();
+  }
+
+  void Update() {
+    time -= Time.deltaTime;
+    if (time < 0)
+      time = 0;
+    GetComponent<Text>().text = ((int)time).ToString();
+  }
+
+  private float time = 60;
+}
