ee38129c3323485ca81aec2314103064110cbcc5
siun <siuncyclone@gmail.com>
2016-11-11 16:44:54 +0900

    Job to Skill, Using anim instead of sprite in Skill

---
+++ Assets/Scripts/Skill.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class Skill : MonoBehaviour {
+  protected IEnumerator DelayMethod(float waitTime, Action action) {
+    yield return new WaitForSeconds(waitTime);
+    action();
+  }
+
+  public void SkillX() {
+    if (Input.GetKey(KeyCode.X))
+      _anim.SetBool("SkillX", true);
+    else
+      _anim.SetBool("SkillX", false);
+  }
+
+  public void SkillShift() {
+    if (Input.GetKey(KeyCode.LeftShift))
+      _anim.SetBool("SkillShift", true);
+    else
+      _anim.SetBool("SkillShift", false);
+    /*
+    GetComponent<SpriteRenderer>().sprite = _actionShift;
+    StartCoroutine(DelayMethod(0.5f, () => {
+      GetComponent<SpriteRenderer>().sprite = _actionNormal;
+    }));
+    */
+  }
+
+  void Update() {
+    SkillX();
+    SkillShift();
+  }
+
+  [SerializeField] private Animator _anim;
+}
