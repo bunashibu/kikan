2bf2112071e65cf0958de053642e77c36e223c18
siun <siuncyclone@gmail.com>
2017-01-14 15:00:57 +0900

    Add: RigidState

---
+++ Assets/Scripts/Movement/RigidState.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RigidState : MonoBehaviour {
+  public bool Ground {
+    get {
+      return true;
+    }
+  }
+
+  public bool Air { get; private set; }
+
+  public bool Stand {
+    get {
+      return true;
+    }
+  }
+
+  public bool LieDown { get; private set; }
+  public bool Ladder { get; private set; }
+  public bool Slow { get; private set; }
+  public bool Heavy { get; private set; }
+  public bool Immobile { get; private set; }
+
+  [SerializeField] private Rigidbody2D _rigid;
+}
+
