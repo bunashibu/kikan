481acbb12a60ecad271575b232eb40f17b6c5249
siun <siuncyclone@gmail.com>
2017-01-15 00:00:59 +0900

    Impl: Climb System/Manipulator Prototype

---
+++ Assets/Scripts/Movement/Manipulator/Manual/ManualClimbManipulator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ManualClimbManipulator : MonoBehaviour {
+  void Update() {
+    if (_system.CanUse) {
+      if (Input.GetKey(KeyCode.UpArrow))
+        _system.MoveUp();
+
+      if (Input.GetKey(KeyCode.DownArrow))
+        _system.MoveDown();
+    }
+  }
+
+  [SerializeField] private ClimbSystem _system;
+}
+
--- Assets/Scripts/Movement/System/ClimbSystem.cs
+++ Assets/Scripts/Movement/System/ClimbSystem.cs
@@ -2,5 +2,38 @@
 using System.Collections;
 
 public class ClimbSystem : MonoBehaviour {
+  void FixedUpdate() {
+    if (_actFlag) {
+      _rigid.AddForce(_inputVec * 30.0f);
+      _actFlag = false;
+    }
+  }
+
+  public void MoveUp() {
+    _actFlag = true;
+    _inputVec.y += 1;
+
+    if (_inputVec.y > 1)
+      _inputVec.y = 1;
+  }
+
+  public void MoveDown() {
+    _actFlag = true;
+    _inputVec.y -= 1;
+
+    if (_inputVec.y < -1)
+      _inputVec.y = -1;
+  }
+
+  public bool CanUse {
+    get {
+      return _state.Ladder;
+    }
+  }
+
+  [SerializeField] private Rigidbody2D _rigid;
+  [SerializeField] private RigidState _state;
+  private bool _actFlag;
+  private Vector2 _inputVec;
 }
 
