6cb479ea1a6e908e33b039aaa09cc5979b113049
siun <siuncyclone@gmail.com>
2017-02-16 16:45:09 +0900

    Show Player Name

---
+++ Assets/Scripts/GetPlayerName.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GetPlayerName : MonoBehaviour {
+  void Start() {
+    _text.text = PhotonNetwork.playerName;
+  }
+
+  [SerializeField] private Text _text;
+}
+
