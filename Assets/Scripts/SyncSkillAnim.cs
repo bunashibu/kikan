83f34dc056a03a70bc48ed1ccacb159252d42c61
siun <siuncyclone@gmail.com>
2017-04-30 16:18:54 +0900

    Sync Skill pos, offset, flipX

---
+++ Assets/Scripts/SyncSkillAnim.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncSkillAnim : MonoBehaviour {
+  void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+    if (stream.isWriting) {
+      stream.SendNext(_renderer.flipX);
+    } else {
+      _renderer.flipX = (bool)stream.ReceiveNext();
+    }
+  }
+
+  [SerializeField] private SpriteRenderer _renderer;
+}
+
