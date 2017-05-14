0be828d782b9957fc15ca52df8ddd76a3943f25d
siun <siuncyclone@gmail.com>
2017-05-14 22:14:12 +0900

    Change HUDExpBar color, Add RatioBar

---
+++ Assets/Scripts/Bar/RatioBar.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RatioBar : Bar {
+  public override void Show(int cur, int max) {
+    _text.text = cur + " [" + cur / max * 100.0 + "%]  ";
+    Animate(cur, max);
+  }
+
+  [SerializeField] private Text _text;
+}
+
