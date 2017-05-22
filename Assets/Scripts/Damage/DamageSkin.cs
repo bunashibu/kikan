610e1cb02bc5b169d7f6d17d8d05f532a575c38a
siun <siuncyclone@gmail.com>
2017-05-22 18:14:44 +0900

    Add DamageSkin

---
+++ Assets/Scripts/Damage/DamageSkin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class DamageSkin : ScriptableObject {
+  public ReadOnlyCollection<Sprite> Hit {
+    get {
+      return Array.AsReadOnly(_hit);
+    }
+  }
+
+  public ReadOnlyCollection<Sprite> Critical {
+    get {
+      return Array.AsReadOnly(_critical);
+    }
+  }
+
+  public ReadOnlyCollection<Sprite> Take {
+    get {
+      return Array.AsReadOnly(_take);
+    }
+  }
+
+  public ReadOnlyCollection<Sprite> Heal {
+    get {
+      return Array.AsReadOnly(_heal);
+    }
+  }
+
+  [SerializeField] private Sprite[] _hit;
+  [SerializeField] private Sprite[] _critical;
+  [SerializeField] private Sprite[] _take;
+  [SerializeField] private Sprite[] _heal;
+}
+
