2c74aa8ee491b66386e461d236b4fd26c7182a86
siun <siuncyclone@gmail.com>
2017-05-24 22:09:10 +0900

    Fix: Display correct exp percent

--- Assets/Scripts/Bar/RatioBar.cs
+++ Assets/Scripts/Bar/RatioBar.cs
@@ -1,11 +1,15 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class RatioBar : Bar {
   public override void Show(int cur, int max) {
-    _text.text = cur + " [" + cur / max * 100.0 + "%]  ";
+    double ratio = (double)cur / max * 100.0;
+    double percent = Math.Round(ratio, 2);
+
+    _text.text = cur + " [" + percent + "%]  ";
     Animate(cur, max);
   }
 
