9600b9a1ab85d2d8fb03659043ebdf4de4e6248b
siun <siuncyclone@gmail.com>
2017-05-27 11:31:47 +0900

    Add Gold

---
+++ Assets/Scripts/Gold/Gold.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gold : Photon.MonoBehaviour, IGauge<int> {
+  [PunRPC]
+  protected void SyncGoldInit() {
+    Cur = 0;
+    Min = 0;
+    Max = 99999;
+  }
+
+  [PunRPC]
+  protected void SyncGoldCur(int cur) {
+    Cur = cur;
+  }
+
+  public void Plus(int quantity) {
+    Cur += quantity;
+
+    if (Cur > Max)
+      Cur = Max;
+    if (Cur < Min)
+      Cur = Min;
+
+    photonView.RPC("SyncGoldCur", PhotonTargets.Others, Cur);
+  }
+
+  public void Minus(int quantity) {
+    Plus(-quantity);
+  }
+
+  public int Cur { get; private set; }
+  public int Min { get; private set; }
+  public int Max { get; private set; }
+}
+
