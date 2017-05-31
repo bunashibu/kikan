568774b6259eb74d6c0b6083f60dc448706ff7d5
siun <siuncyclone@gmail.com>
2017-05-31 22:22:59 +0900

    Set CorePanel inspector

--- Assets/Scripts/Core/CorePanel.cs
+++ Assets/Scripts/Core/CorePanel.cs
@@ -3,12 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CorePanel : MonoBehaviour {
+  public CoreChart Speed {
+    get {
+      return _speedChart;
+    }
+  }
+
+  public CoreChart Hp {
+    get {
+      return _hpChart;
+    }
+  }
+
+  public CoreChart Attack {
+    get {
+      return _attackChart;
+    }
+  }
+
   public CoreChart Critical {
     get {
       return _criticalChart;
     }
   }
 
+  public CoreChart Heal {
+    get {
+      return _healChart;
+    }
+  }
+
+  [SerializeField] private CoreChart _speedChart;
+  [SerializeField] private CoreChart _hpChart;
+  [SerializeField] private CoreChart _attackChart;
   [SerializeField] private CoreChart _criticalChart;
+  [SerializeField] private CoreChart _healChart;
 }
 
