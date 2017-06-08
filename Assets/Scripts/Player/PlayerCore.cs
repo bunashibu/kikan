59dc482c48310f58669f2df44e5dd31118b1a1d0
siun <siuncyclone@gmail.com>
2017-06-08 11:14:10 +0900

    Generalize PlayerCore LvUp

--- Assets/Scripts/Player/PlayerCore.cs
+++ Assets/Scripts/Player/PlayerCore.cs
@@ -3,30 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerCore : Photon.MonoBehaviour {
+  void Awake() {
+    _reconfirmList = new List<bool>() { false, false, false, false, false };
+  }
+
   void Update() {
     if (photonView.isMine) {
-      bool lvUpCriticalRequest = Input.GetKeyDown(_criticalCore.Key);
+      for (int i=0; i<_coreList.Count; ++i) {
+        bool lvUpRequest = Input.GetKeyDown(_coreList[i].Key);
 
-      if (lvUpCriticalRequest) {
+        // HACK: Too dirty
+        if (lvUpRequest) {
+          if (_reconfirmList[i]) {
+            bool isAffordable = (_playerGold.Cur >= _coreList[i].Gold);
 
-        if(_isReconfirming) {
+            if (isAffordable) {
+              _playerGold.Minus(_coreList[i].Gold);
+              _playerGold.UpdateView();
 
-          _isAffordable = (_playerGold.Cur >= _criticalCore.Gold);
-
-          if (_isAffordable) {
-            _playerGold.Minus(_criticalCore.Gold);
-            _playerGold.UpdateView();
-
-            _criticalCore.LvUp();
-            UpdateCriticalView(_criticalCore.Level);
-            _isReconfirming = false;
-
+              _coreList[i].LvUp();
+              UpdateView(i, _coreList[i].Level);
+              _reconfirmList[i] = false;
+            } else {
+              Debug.Log("You don't have enough gold.");
+            }
           } else {
-            Debug.Log("You don't have enough gold.");
+            _reconfirmList[i] = true;
           }
-
-        } else {
-          _isReconfirming = true;
         }
       }
     }
@@ -36,31 +39,63 @@ public class PlayerCore : Photon.MonoBehaviour {
     _corePanel = corePanel;
   }
 
-  private void UpdateAttackView(int level) {
-    _corePanel.Attack.UpdateView(level);
+  // HACK: Maybe it should use array
+  private void UpdateView(int index, int level) {
+    switch (index) {
+      case 0:
+        _corePanel.Speed.UpdateView(level);
+        break;
+      case 1:
+        _corePanel.Hp.UpdateView(level);
+        break;
+      case 2:
+        _corePanel.Attack.UpdateView(level);
+        break;
+      case 3:
+        _corePanel.Critical.UpdateView(level);
+        break;
+      case 4:
+        _corePanel.Heal.UpdateView(level);
+        break;
+      default:
+        Debug.Log("Error: PlayerCore-UpdateView argument-index is wrong");
+        break;
+    }
   }
 
-  private void UpdateCriticalView(int level) {
-    _corePanel.Critical.UpdateView(level);
+  public int Speed {
+    get {
+      return _coreList[0].Value;
+    }
+  }
+
+  public int Hp {
+    get {
+      return _coreList[1].Value;
+    }
   }
 
   public int Attack {
     get {
-      return _attackCore.Value;
+      return _coreList[2].Value;
     }
   }
 
   public int Critical {
     get {
-      return _criticalCore.Value;
+      return _coreList[3].Value;
+    }
+  }
+
+  public int Heal {
+    get {
+      return _coreList[4].Value;
     }
   }
 
   [SerializeField] private PlayerGold _playerGold;
-  [SerializeField] private Core _attackCore;
-  [SerializeField] private Core _criticalCore;
+  [SerializeField] private List<Core> _coreList;
   private CorePanel _corePanel;
-  private bool _isReconfirming = false;
-  private bool _isAffordable = false;
+  private List<bool> _reconfirmList;
 }
 
