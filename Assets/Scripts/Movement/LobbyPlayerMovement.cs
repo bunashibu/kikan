8e8262da66717d26491610c97bd28a5d283c8c48
siun <siuncyclone@gmail.com>
2017-06-18 22:11:09 +0900

    Add LobbyPlayer

---
+++ Assets/Scripts/Movement/LobbyPlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyPlayerMovement {
+  public void AirMoveLeft() {
+    _airLinear.MoveLeft();
+  }
+
+  public void AirMoveRight() {
+    _airLinear.MoveRight();
+  }
+
+  public void GroundMoveLeft() {
+    _groundLinear.MoveLeft();
+  }
+
+  public void GroundMoveRight() {
+    _groundLinear.MoveRight();
+  }
+
+  public void GroundJump() {
+    _groundJump.Jump();
+  }
+
+  public void ClimbJump() {
+    _climbJump.JumpOff();
+  }
+
+  public void StepDownJump() {
+    _stepDownJump.StepDown();
+  }
+
+  public void ClimbUp() {
+    _climb.MoveUp();
+  }
+
+  public void ClimbDown() {
+    _climb.MoveDown();
+  }
+
+  public void LieDown() {
+    _lieDown.Lie();
+  }
+
+  public void Stand() {
+    _lieDown.Stand();
+  }
+
+  private AirLinearMove _airLinear;
+  private GroundLinearMove _groundLinear;
+
+  private GroundJump _groundJump;
+  private ClimbJump _climbJump;
+  private StepDownJump _stepDownJump;
+
+  private Climb _climb;
+  private LieDown _lieDown;
+}
+
---
+++ Assets/Scripts/Player/LobbyPlayer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LobbyPlayer : Photon.MonoBehaviour {
+  void Awake() {
+    Movement = new LobbyPlayerMovement();
+  }
+
+  public LobbyPlayerMovement Movement { get; private set; }
+}
+
