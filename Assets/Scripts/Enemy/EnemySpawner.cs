c80a1478ce43a5cb727865bd94cf8a961d2148a0
siun <siuncyclone@gmail.com>
2017-07-08 11:30:31 +0900

    Update EnemySpawner

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -8,10 +8,27 @@ public class EnemySpawner : MonoBehaviour {
   void Awake() {
     float ratioSum = _spawnRatio.Aggregate((x, y) => x + y);
     Assert.AreApproximatelyEqual(ratioSum, 1.0f, 0.001f);
+
+    NetworkSpawn();
   }
 
-  public void NetworkSpawn(string enemyName, Vector3 pos) {
-    PhotonNetwork.Instantiate("Prehabs/Enemy/" + enemyName, pos, Quaternion.identity, 0);
+  public void NetworkSpawn() {
+    var pos = new Vector3(0.0f, 0.0f, 0.0f);
+
+    int index = 0;
+    var threshold = Random.value;
+    Debug.Log(threshold);
+
+    _spawnRatio.Aggregate((probability, x) => {
+      if (probability > threshold) {
+        return probability;
+      } else {
+        index += 1;
+        return probability + x;
+      }
+    });
+
+    PhotonNetwork.Instantiate("Prefabs/Enemy/" + _spawnEnemyNames[index], pos, Quaternion.identity, 0);
   }
 
   [SerializeField] private string[] _spawnEnemyNames;
