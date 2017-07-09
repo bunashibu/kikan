3bb57d81268aceb5a56f784a4add6ee2e1b52b55
siun <siuncyclone@gmail.com>
2017-07-10 00:07:50 +0900

    Update Enemy's KillReward

--- Assets/Scripts/Enemy/Enemy.cs
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -3,9 +3,16 @@ using System;
 using System.Collections;
 
 [RequireComponent(typeof(EnemyObserver))]
-public class Enemy : MonoBehaviour {
+public class Enemy : MonoBehaviour, IKillReward {
   public PhotonView PhotonView { get { return _photonView; } }
 
+  public int KillExp  { get { return _killExp;  } }
+  public int KillGold { get { return _killGold; } }
+
   [SerializeField] private PhotonView _photonView;
+
+  [Header("Kill Reward")]
+  [SerializeField] private int _killExp;
+  [SerializeField] private int _killGold;
 }
 
