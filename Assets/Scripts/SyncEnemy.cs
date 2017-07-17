b87952cf331142483a1d6dd79abe8ff4ce668545
siun <siuncyclone@gmail.com>
2017-07-17 15:58:18 +0900

    Add SyncEnemy

---
+++ Assets/Scripts/SyncEnemy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SyncEnemy : MonoBehaviour {
+  void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+    if (stream.isWriting) {
+      stream.SendNext(_renderer.flipX);
+      stream.SendNext(_anim.GetBool("Idle"));
+      stream.SendNext(_anim.GetBool("Die"));
+    } else {
+      _renderer.flipX = (bool)stream.ReceiveNext();
+      _anim.SetBool("Idle", (bool)stream.ReceiveNext());
+      _anim.SetBool("Die", (bool)stream.ReceiveNext());
+    }
+  }
+
+  [SerializeField] private SpriteRenderer _renderer;
+  [SerializeField] private Animator _anim;
+}
+
