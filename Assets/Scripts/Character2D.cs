36b604996a7b2d43c1b37a15e973054675f2b204
siun <siuncyclone@gmail.com>
2017-09-27 21:42:13 +0900

    Update Character2D: hitGround

--- Assets/Scripts/Character2D.cs
+++ Assets/Scripts/Character2D.cs
@@ -11,7 +11,24 @@ public class Character2D : MonoBehaviour {
 
   private void UpdateRaycast() {
     Vector2 footRayOrigin = new Vector2(_character.Collider.bounds.center.x, _character.Collider.bounds.min.y);
-    RaycastHit2D hit = Physics2D.Raycast(footRayOrigin, Vector2.down, Mathf.Abs(_character.Rigid.velocity.y), _groundMask);
+    RaycastHit2D hitGround = Physics2D.Raycast(footRayOrigin, Vector2.down, Mathf.Abs(_character.Rigid.velocity.y), _groundMask);
+
+    if (hitGround.collider != null) {
+      Debug.DrawRay(footRayOrigin, Vector2.down * Mathf.Abs(_character.Rigid.velocity.y), Color.red);
+      CalculateGroundDistance(hitGround);
+    } else {
+      _character.Rigid.gravityScale = 1;
+    }
+  }
+
+  private void CalculateGroundDistance(RaycastHit2D hitGround) {
+    if (hitGround.distance == 0) {
+      _character.Rigid.velocity = new Vector2(_character.Rigid.velocity.x, 0);
+      _character.Rigid.gravityScale = 0;
+      transform.position = new Vector3(transform.position.x, hitGround.collider.bounds.max.y + _character.Collider.bounds.extents.y, 0);
+    } else {
+      _character.Rigid.gravityScale = 1;
+    }
   }
 
   [SerializeField] private ICharacter _character;
