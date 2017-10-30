db23c75a04eb6433f06485e0330150a9f5313f14
siun <siuncyclone@gmail.com>
2017-10-30 22:25:27 +0900

    Fix Climb -> Ladder

--- Assets/Scripts/SyncBattlePlayer.cs
+++ Assets/Scripts/SyncBattlePlayer.cs
@@ -10,10 +10,10 @@ namespace Bunashibu.Kikan {
         stream.SendNext(_anim.GetBool("Idle"       ));
         stream.SendNext(_anim.GetBool("Fall"       ));
         stream.SendNext(_anim.GetBool("Walk"       ));
-        stream.SendNext(_anim.GetBool("Climb"      ));
+        stream.SendNext(_anim.GetBool("Ladder"     ));
         stream.SendNext(_anim.GetBool("LieDown"    ));
         stream.SendNext(_anim.GetBool("GroundJump" ));
-        stream.SendNext(_anim.GetBool("ClimbJump"  ));
+        stream.SendNext(_anim.GetBool("LadderJump" ));
         stream.SendNext(_anim.GetBool("Skill"      ));
         stream.SendNext(_anim.GetBool("Die"        ));
       } else {
@@ -21,10 +21,10 @@ namespace Bunashibu.Kikan {
         _anim.SetBool("Idle"       , (bool)stream.ReceiveNext());
         _anim.SetBool("Fall"       , (bool)stream.ReceiveNext());
         _anim.SetBool("Walk"       , (bool)stream.ReceiveNext());
-        _anim.SetBool("Climb"      , (bool)stream.ReceiveNext());
+        _anim.SetBool("Ladder"     , (bool)stream.ReceiveNext());
         _anim.SetBool("LieDown"    , (bool)stream.ReceiveNext());
         _anim.SetBool("GroundJump" , (bool)stream.ReceiveNext());
-        _anim.SetBool("ClimbJump"  , (bool)stream.ReceiveNext());
+        _anim.SetBool("LadderJump" , (bool)stream.ReceiveNext());
         _anim.SetBool("Skill"      , (bool)stream.ReceiveNext());
         _anim.SetBool("Die"        , (bool)stream.ReceiveNext());
       }
