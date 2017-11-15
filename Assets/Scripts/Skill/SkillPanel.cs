7b190e0065f010e63cf9609b6b901762dd1308e1
siun <siuncyclone@gmail.com>
2017-11-15 23:44:41 +0900

    Update UI: ManjiSkillPanel

---
+++ Assets/Scripts/Skill/SkillPanel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bunashibu.Kikan {
+  public class SkillPanel : MonoBehaviour {
+    [SerializeField] private List<Image> _alphaList;
+  }
+}
+
