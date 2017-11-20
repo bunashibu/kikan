1fc8e51d6397055679a3e532988cb6081ba88c81
siun <siuncyclone@gmail.com>
2017-11-20 19:17:25 +0900

    Update TrackCamera

--- Assets/Scripts/Player/TrackCamera.cs
+++ Assets/Scripts/Player/TrackCamera.cs
@@ -6,7 +6,6 @@ namespace Bunashibu.Kikan {
   public class TrackCamera : MonoBehaviour {
     void Start() {
       transform.position = transform.position + _positionOffset;
-      _destination = new Vector3(0, 0, 0);
     }
 
     void Update() {
@@ -14,8 +13,6 @@ namespace Bunashibu.Kikan {
 
       if (transform.position != trackPosition)
         InterpolateTo(trackPosition);
-      else
-        _elapsedTime = 0;
     }
 
     public void Init(GameObject trackObj) {
@@ -23,26 +20,12 @@ namespace Bunashibu.Kikan {
     }
 
     private void InterpolateTo(Vector3 destination) {
-      if (_destination != destination)
-        _elapsedTime = 0;
-
-      _destination = destination;
-      _elapsedTime += Time.deltaTime;
-
-      float t = _elapsedTime / 3.0f;
-      if (t > 1.0f)
-        t = 1.0f;
-
-      float ratio = t * t * (3.0f - 2.0f * t);
-
-      Vector3 nextPosition = transform.position * (1.0f - ratio) + destination * ratio;
+      Vector3 nextPosition = transform.position * 0.98f + destination * 0.02f;
       transform.position = nextPosition;
     }
 
     [SerializeField] private Vector3 _positionOffset;
     private GameObject _trackObj;
-    private Vector3 _destination;
-    private float _elapsedTime;
   }
 }
 
