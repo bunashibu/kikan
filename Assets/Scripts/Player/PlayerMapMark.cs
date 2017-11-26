038329204784401671bca556d5bd5d7f35e5e02b
siun <siuncyclone@gmail.com>
2017-11-26 10:41:54 +0900

    Update: Detect PlayerTeam on Map

---
+++ Assets/Scripts/Player/PlayerMapMark.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class PlayerMapMark : MonoBehaviour {
+    void Start() {
+      if (_player.PhotonView.owner == PhotonNetwork.player) {
+        _renderer.color = Color.yellow;
+        return;
+      }
+
+      int clientTeam = (int)PhotonNetwork.player.CustomProperties["Team"];
+      if (_player.PlayerInfo.Team == clientTeam)
+        _renderer.color = new Color(1.0f, 0.45f, 0.0f, 1.0f); // Orange
+      else
+        _renderer.color = Color.red;
+    }
+
+    [SerializeField] private BattlePlayer _player;
+    [SerializeField] private SpriteRenderer _renderer;
+  }
+}
+
