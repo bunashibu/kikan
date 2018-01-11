d10aee91fb830ea04d3ceb94cd044342d2e63e2c
siun <siuncyclone@gmail.com>
2018-01-11 11:14:22 +0900

    Update

--- Assets/Scripts/FixedCamera.cs
+++ Assets/Scripts/FixedCamera.cs
@@ -4,14 +4,14 @@ using System.Collections;
 namespace Bunashibu.Kikan {
   public class FixedCamera : Photon.MonoBehaviour {
     void Start() {
-      int sign = 1;
       if ((int)PhotonNetwork.player.CustomProperties["Team"] == 1)
-        sign *= -1;
+        _x *= -1;
 
-      transform.position = new Vector3(_gameData.RespawnPosition.x * sign, _gameData.RespawnPosition.y, _z);
+      transform.position = new Vector3(_x, _y, _z);
     }
 
-    [SerializeField] private GameData _gameData;
+    [SerializeField] private float _x;
+    [SerializeField] private float _y;
     [SerializeField] private float _z;
   }
 }
