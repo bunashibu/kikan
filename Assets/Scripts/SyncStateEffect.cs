2353a57af1b603c2bca71c515b7f6d17ada95d7b
siun <siuncyclone@gmail.com>
2018-02-27 13:54:08 +0900

    Add SyncStateEffect

---
+++ Assets/Scripts/SyncStateEffect.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class SyncStateEffect : MonoBehaviour {
+    void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+      if (stream.isWriting) {
+        stream.SendNext(_anim.GetBool("None"));
+        stream.SendNext(_anim.GetBool("Stun"));
+      } else {
+        _anim.SetBool("None", (bool)stream.ReceiveNext());
+        _anim.SetBool("Stun", (bool)stream.ReceiveNext());
+      }
+    }
+
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private Animator _anim;
+  }
+}
+
