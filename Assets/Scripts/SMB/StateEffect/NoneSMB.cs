bc8839e66e0b43108f5664c444439b6eedb5aa3c
siun <siuncyclone@gmail.com>
2018-02-27 13:23:18 +0900

    Add StateEffect

---
+++ Assets/Scripts/SMB/StateEffect/NoneSMB.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class NoneSMB : StateMachineBehaviour {
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+      if (_character == null) {
+        _character = animator.transform.parent.GetComponent<ICharacter>();
+        _stateEffect = animator.GetComponent<StateEffect>();
+        _transform = animator.GetComponent<Transform>();
+      }
+
+      _transform.position = _transform.parent.position;
+    }
+
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+      if (_character.State.Stun) { _stateEffect.StateTransfer.TransitTo("Stun", animator); }
+    }
+
+    private ICharacter _character;
+    private StateEffect _stateEffect;
+    private Transform _transform;
+  }
+}
+
---
+++ Assets/Scripts/StateEffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class StateEffect : MonoBehaviour {
+    void Awake() {
+      StateTransfer = new StateTransfer("None", _animator);
+    }
+
+    public StateTransfer StateTransfer { get; private set; }
+
+    [SerializeField] private Animator _animator;
+  }
+}
+
