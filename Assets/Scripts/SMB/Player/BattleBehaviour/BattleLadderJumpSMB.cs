36095d05f061244879242c0a2ff7e2eeffa0d875
siun <siuncyclone@gmail.com>
2018-04-05 17:38:24 +0900

    Fix freeze when ladderjump to ground idle

--- Assets/Scripts/SMB/Player/BattleBehaviour/BattleLadderJumpSMB.cs
+++ Assets/Scripts/SMB/Player/BattleBehaviour/BattleLadderJumpSMB.cs
@@ -13,9 +13,10 @@ namespace Bunashibu.Kikan {
 
     override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
       if (_player.PhotonView.isMine) {
-        if ( _player.Hp.Cur <= 0                        ) { _player.StateTransfer.TransitTo( "Die"  , animator ); return; }
-        if ( _player.BuffState.Stun                     ) { _player.StateTransfer.TransitTo( "Stun" , animator ); return; }
-        if ( _player.State.Air && !_player.State.Ladder ) { _player.StateTransfer.TransitTo( "Fall" , animator ); return; }
+        if ( _player.Hp.Cur <= 0                           ) { _player.StateTransfer.TransitTo( "Die"  , animator ); return; }
+        if ( _player.BuffState.Stun                        ) { _player.StateTransfer.TransitTo( "Stun" , animator ); return; }
+        if ( _player.State.Air && !_player.State.Ladder    ) { _player.StateTransfer.TransitTo( "Fall" , animator ); return; }
+        if ( _player.State.Ground && !_player.State.Ladder ) { _player.StateTransfer.TransitTo( "Idle" , animator ); return; }
       }
     }
 
