7396474d4e991a9f76c2a33df3a3453591027fc8
siun <siuncyclone@gmail.com>
2018-04-06 14:49:10 +0900

    Impl Alt

---
+++ Assets/Scripts/Skill/All/Alt.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class Alt : Skill {
+    void Start() {
+      var skillUser = _skillUserObj.GetComponent<BattlePlayer>();
+      skillUser.Hp.Add(2000);
+      skillUser.Hp.UpdateView();
+    }
+  }
+}
+
