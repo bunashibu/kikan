bff1494700fd68b149a3abd5819bc0bbdbea811f
siun <siuncyclone@gmail.com>
2018-04-06 16:24:13 +0900

    Impl HealArea

---
+++ Assets/Scripts/HealArea.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class HealArea : MonoBehaviour {
+    void OnTriggerStay2D(Collider2D collider) {
+      var targetObj = collider.gameObject;
+      if (targetObj.tag != "Player")
+        return;
+
+      var target = targetObj.GetComponent<BattlePlayer>();
+
+      if (!target.PhotonView.isMine)
+        return;
+
+      if (target.PlayerInfo.Team == _team)
+        target.Hp.FullRecover();
+      else
+        target.Hp.Subtract(1000);
+
+      target.Hp.UpdateView();
+    }
+
+    [SerializeField] private int _team;
+  }
+}
+
