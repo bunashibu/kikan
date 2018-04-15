85e20688aaebfa2aee2e617e847c2174cba092c3
siun <siuncyclone@gmail.com>
2018-04-15 14:56:45 +0900

    Fix sync ManjiSpace

--- Assets/Scripts/SkillReference.cs
+++ Assets/Scripts/SkillReference.cs
@@ -5,26 +5,26 @@ using UnityEngine;
 namespace Bunashibu.Kikan {
   public class SkillReference : MonoBehaviour {
     void Awake() {
-      _existSkillList = new List<Skill>();
+      _existOwnSkillList = new List<Skill>();
     }
 
     public void Register(Skill skill) {
-      _existSkillList.Add(skill);
+      _existOwnSkillList.Add(skill);
     }
 
     public void Remove(Skill skill) {
-      _existSkillList.Remove(skill);
+      _existOwnSkillList.Remove(skill);
     }
 
     public void DeleteAll() {
-      foreach (Skill skill in _existSkillList) {
-        Destroy(skill.gameObject);
+      foreach (Skill skill in _existOwnSkillList) {
+        PhotonNetwork.Destroy(skill.gameObject);
       }
 
-      _existSkillList = new List<Skill>();
+      _existOwnSkillList = new List<Skill>();
     }
 
-    private List<Skill> _existSkillList;
+    private List<Skill> _existOwnSkillList;
   }
 }
 
