640582193199c9d921c8ba526e197f307061b616
siun <siuncyclone@gmail.com>
2018-05-24 20:38:51 +0900

    Fix FPS to 60

---
+++ Assets/Scripts/Singleton/FPSManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class FPSManager : SingletonMonoBehaviour<FPSManager> {
+    void Awake() {
+      Application.targetFrameRate = 60;
+    }
+  }
+}
+
