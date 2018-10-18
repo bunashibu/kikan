fb523c9466fc76e565d4bd2007384c0b4c797e62
siun <siuncyclone@gmail.com>
2018-10-18 22:49:39 +0900

    Add IAttacker, INotifier

---
+++ Assets/Scripts/Interface/IAttacker.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public interface IAttacker {
+    int Power    { get; }
+    int Critical { get; }
+  }
+}
+
