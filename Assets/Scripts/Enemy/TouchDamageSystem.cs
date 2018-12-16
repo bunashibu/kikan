6ecb6dc2619c08c26c94d731290c802b6cda8d1b
siun <siuncyclone@gmail.com>
2018-12-16 17:01:02 +0900

    Sync TouchDamageSystem; Clean Folder

--- Assets/Scripts/Enemy/TouchDamageSystem.cs
+++ Assets/Scripts/Enemy/TouchDamageSystem.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 namespace Bunashibu.Kikan {
   public class TouchDamageSystem : MonoBehaviour {
+    void Awake() {
+      _synchronizer = GetComponent<AttackSynchronizer>();
+    }
+
     void OnTriggerStay2D(Collider2D collider) {
       var targetObj = collider.gameObject;
       if (targetObj.tag != "Player")
         return;
 
       var target = targetObj.GetComponent<Player>();
-
       if (!target.PhotonView.isMine)
         return;
-
       if (target.State.Invincible)
         return;
 
       var damage = _baseDamage + (int)(Random.value * _positiveDeviation);
+      var attackerViewID = GetComponent<IPhoton>().PhotonView.viewID;
 
-      //tmp
-      var attacker = GetComponent<IAttacker>();
-      target.OnAttacked(attacker, damage, false, HitEffectType.None);
+      _synchronizer.SyncAttack(attackerViewID, target.PhotonView.viewID, damage, false, HitEffectType.None);
 
       target.State.Invincible = true;
       MonoUtility.Instance.DelaySec(2.0f, () => { target.State.Invincible = false; } );
@@ -29,6 +30,7 @@ namespace Bunashibu.Kikan {
 
     [SerializeField] private int _baseDamage;
     [SerializeField] private int _positiveDeviation;
+    private AttackSynchronizer _synchronizer;
   }
 }
 
