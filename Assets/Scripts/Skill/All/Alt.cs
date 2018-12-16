cdcbfcbc632eadd40f9b69217c55cfe1bf648873
siun <siuncyclone@gmail.com>
2018-12-16 18:41:45 +0900

    Add HealSynchronizer

--- Assets/Scripts/Skill/All/Alt.cs
+++ Assets/Scripts/Skill/All/Alt.cs
@@ -4,15 +4,21 @@ using UnityEngine;
 
 namespace Bunashibu.Kikan {
   public class Alt : Skill {
+    void Awake() {
+      _synchronizer = GetComponent<HealSynchronizer>();
+    }
+
     void Start() {
       if (!photonView.isMine)
         return;
 
       var skillUser = _skillUserObj.GetComponent<Player>();
-
       int addition = skillUser.Level.Cur.Value * 50 + (int)(Random.value * 100);
-      skillUser.Hp.Add(2000 + addition);
+
+      _synchronizer.SyncHeal(_skillUserViewID, 2000 + addition);
     }
+
+    private HealSynchronizer _synchronizer;
   }
 }
 
