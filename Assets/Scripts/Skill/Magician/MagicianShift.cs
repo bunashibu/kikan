202c057cc0d5995a9551ac20284f5d8254a78904
siun <siuncyclone@gmail.com>
2018-12-26 14:43:22 +0900

    Add MagicianShift

---
+++ Assets/Scripts/Skill/Magician/MagicianShift.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Bunashibu.Kikan {
+  [RequireComponent(typeof(SkillSynchronizer))]
+  public class MagicianShift : Skill {
+    void Awake() {
+      _synchronizer = GetComponent<SkillSynchronizer>();
+      _targetChecker = new TargetChecker(_targetNum);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+      if (PhotonNetwork.isMasterClient && _targetChecker.IsAttackTarget(collider, _skillUserObj)) {
+        DamageCalculator.Calculate(_skillUserObj, _attackInfo);
+
+        var target = collider.gameObject.GetComponent<IPhoton>();
+        Assert.IsNotNull(target);
+
+        _synchronizer.SyncAttack(_skillUserViewID, target.PhotonView.viewID, DamageCalculator.Damage, DamageCalculator.IsCritical, HitEffectType.Magician);
+      }
+    }
+
+    [SerializeField] private AttackInfo _attackInfo;
+    [SerializeField] private int _targetNum;
+
+    private SkillSynchronizer _synchronizer;
+    private TargetChecker _targetChecker;
+  }
+}
+
