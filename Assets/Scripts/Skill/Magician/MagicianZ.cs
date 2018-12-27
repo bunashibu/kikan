9f72c1f5e0c7932f3e49ee963eb0d949e499a6e0
siun <siuncyclone@gmail.com>
2018-12-27 15:28:26 +0900

    Add MagicianZ

---
+++ Assets/Scripts/Skill/Magician/MagicianZ.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  [RequireComponent(typeof(SkillSynchronizer))]
+  public class MagicianZ : Skill {
+    void Awake() {
+      _synchronizer = GetComponent<SkillSynchronizer>();
+    }
+
+    void OnTriggerStay2D(Collider2D collider) {
+    }
+
+    private SkillSynchronizer _synchronizer;
+  }
+}
+
