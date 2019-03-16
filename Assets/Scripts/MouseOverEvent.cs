a9a56f669c2c1289eb7766364465c65019b06d1f
siun <siuncyclone@gmail.com>
2019-03-16 22:21:17 +0900

    Add MouseOver SE

---
+++ Assets/Scripts/MouseOverEvent.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  [RequireComponent(typeof(AudioSource))]
+  public class MouseOverEvent : MonoBehaviour {
+    void Awake() {
+      _source = GetComponent<AudioSource>();
+    }
+
+    public void PlayMouseOverSE(AudioClip clip) {
+      _source.PlayOneShot(clip, 0.5f);
+    }
+
+    private AudioSource _source;
+  }
+}
+
