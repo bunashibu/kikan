9cf62797c4ab79503e75c1555b7fdf3220e260a9
siun <siuncyclone@gmail.com>
2019-04-02 17:19:48 +0900

    Add Mixier and Settings

---
+++ Assets/Scripts/Settings/SettingsPanel.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Bunashibu.Kikan {
+  public class SettingsPanel : MonoBehaviour {
+    void Awake() {
+      _button = GetComponent<Button>();
+
+      _button.onClick.AddListener(() => {
+        if (_isOpened)
+          CloseMenu();
+        else
+          OpenMenu();
+      });
+    }
+
+    private void OpenMenu() {
+      _isOpened = true;
+
+      foreach (var menu in _menus)
+        menu.SetActive(true);
+
+    }
+
+    private void CloseMenu() {
+      _isOpened = false;
+
+      foreach (var menu in _menus)
+        menu.SetActive(false);
+
+    }
+
+    [SerializeField] private List<GameObject> _menus;
+    private Button _button;
+    private bool _isOpened;
+  }
+}
+
