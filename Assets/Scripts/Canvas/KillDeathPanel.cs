b5a6cdcf273dbef14d720aa6aa30d0de67118d10
siun <siuncyclone@gmail.com>
2019-04-03 15:30:36 +0900

    Clean KillDeathPanel

--- Assets/Scripts/Canvas/KillDeathPanel.cs
+++ Assets/Scripts/Canvas/KillDeathPanel.cs
@@ -62,50 +62,6 @@ namespace Bunashibu.Kikan {
       }
     }
 
-    public void UpdateNameView(PhotonPlayer player) {
-      photonView.RPC("SyncKDPanelName", PhotonTargets.All, player);
-    }
-
-    public void UpdateLvView(int lv) {
-      photonView.RPC("SyncKDPanelLv", PhotonTargets.All, lv, PhotonNetwork.player);
-    }
-
-    public void UpdateKillView(int kill, PhotonPlayer player) {
-      photonView.RPC("SyncKDPanelKill", PhotonTargets.All, kill, player);
-    }
-
-    public void UpdateDeathView(int death, PhotonPlayer player) {
-      photonView.RPC("SyncKDPanelDeath", PhotonTargets.All, death, player);
-    }
-
-    [PunRPC]
-    private void SyncKDPanelName(PhotonPlayer player) {
-      int team = _playerCellInfo[player].team;
-      int index = _playerCellInfo[player].index;
-      _teamPanels[team].UpdateNameView(player.NickName, index);
-    }
-
-    [PunRPC]
-    private void SyncKDPanelLv(int lv, PhotonPlayer player) {
-      int team = _playerCellInfo[player].team;
-      int index = _playerCellInfo[player].index;
-      _teamPanels[team].UpdateLvView(lv, index);
-    }
-
-    [PunRPC]
-    private void SyncKDPanelKill(int kill, PhotonPlayer player) {
-      int team = _playerCellInfo[player].team;
-      int index = _playerCellInfo[player].index;
-      _teamPanels[team].UpdateKillView(kill, index);
-    }
-
-    [PunRPC]
-    private void SyncKDPanelDeath(int death, PhotonPlayer player) {
-      int team = _playerCellInfo[player].team;
-      int index = _playerCellInfo[player].index;
-      _teamPanels[team].UpdateDeathView(death, index);
-    }
-
     [SerializeField] private KillDeathTeamPanel[] _teamPanels;
     private Dictionary<PhotonPlayer, CellInfo> _playerCellInfo;
   }
