cc982ead4ca13aab81737b125b9b6ff2871637e6
siun <siuncyclone@gmail.com>
2019-04-04 14:51:42 +0900

    Impl NageShift

--- Assets/Scripts/Skill/Nage/NageShift.cs
+++ Assets/Scripts/Skill/Nage/NageShift.cs
@@ -24,7 +24,7 @@ namespace Bunashibu.Kikan {
     private void EnhanceStatus() {
       var skillUser = _skillUserObj.GetComponent<Player>();
 
-      skillUser.Status.SetFixCritical(10);
+      skillUser.Status.SetFixCritical(100);
 
       ResetStatus = () => {
         skillUser.Status.SetFixCritical(0);
@@ -38,12 +38,13 @@ namespace Bunashibu.Kikan {
 
       var buff = PhotonNetwork.Instantiate("Prefabs/Skill/Nage/ShiftBuff", Vector3.zero, Quaternion.identity, 0).GetComponent<SkillBuff>() as SkillBuff;
       buff.ParentSetter.SetParent(skillUser.PhotonView.viewID);
+      buff.transform.Translate(new Vector2(0, 1.1f));
 
       SkillReference.Instance.Register(buff, _buffTime, () => { PhotonNetwork.Destroy(buff.gameObject); });
     }
 
     private Action ResetStatus;
-    private float _buffTime = 5.0f;
+    private float _buffTime = 6.0f;
   }
 }
 
