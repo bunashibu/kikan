9ead13c37872ed8edc226e78252ec716c5c1b64e
siun <siuncyclone@gmail.com>
2019-04-05 20:36:22 +0900

    Flip NageCtrl Image; Impl NageCtrl

---
+++ Assets/Scripts/Skill/Nage/NageCtrl.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Bunashibu.Kikan {
+  [RequireComponent(typeof(SkillSynchronizer))]
+  public class NageCtrl : Skill {
+    void Awake() {
+      _synchronizer = GetComponent<SkillSynchronizer>();
+      _hitRistrictor = new HitRistrictor(_hitInfo);
+
+      MonoUtility.Instance.StoppableDelaySec(_existTime, "NageCtrlFalse" + GetInstanceID().ToString(), () => {
+        gameObject.SetActive(false);
+
+        // NOTE: See SMB-DestroySkillSelf
+        MonoUtility.Instance.StoppableDelaySec(5.0f, "NageCtrlDestroy" + GetInstanceID().ToString(), () => {
+          Destroy(gameObject);
+        });
+      });
+    }
+
+    void Start() {
+      if (photonView.isMine) {
+        var playerRenderer = _skillUserObj.GetComponent<SpriteRenderer>();
+        Assert.IsNotNull(playerRenderer);
+
+        _direction = playerRenderer.flipX ? Vector2.right : Vector2.left;
+      }
+    }
+
+    void Update() {
+      if (photonView.isMine)
+        transform.Translate(_direction * _spd * Time.deltaTime);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+      if (PhotonNetwork.isMasterClient) {
+        var target = collider.gameObject.GetComponent<IPhoton>();
+
+        if (target == null)
+          return;
+        if (TeamChecker.IsSameTeam(collider.gameObject, _skillUserObj))
+          return;
+        if (_hitRistrictor.ShouldRistrict(collider.gameObject))
+          return;
+
+        DamageCalculator.Calculate(_skillUserObj, _attackInfo);
+
+        _synchronizer.SyncAttack(_skillUserViewID, target.PhotonView.viewID, DamageCalculator.Damage, DamageCalculator.IsCritical, HitEffectType.Nage);
+      }
+    }
+
+    void OnDestroy() {
+      if (photonView.isMine && SkillReference.Instance != null)
+        SkillReference.Instance.Remove(this);
+    }
+
+    [SerializeField] private AttackInfo _attackInfo;
+    [SerializeField] private HitInfo _hitInfo;
+
+    private SkillSynchronizer _synchronizer;
+    private HitRistrictor _hitRistrictor;
+    private Vector2 _direction;
+    private float _spd = 8.0f;
+    private float _existTime = 3.0f;
+  }
+}
+
