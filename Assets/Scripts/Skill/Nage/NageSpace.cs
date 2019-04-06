a7f9789f2d634d10cc318ed1da6ab37939435f86
siun <siuncyclone@gmail.com>
2019-04-06 21:43:37 +0900

    Add NageSpace

---
+++ Assets/Scripts/Skill/Nage/NageSpace.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  [RequireComponent(typeof(SkillSynchronizer))]
+  public class NageSpace : Skill {
+    void Awake() {
+      _synchronizer = GetComponent<SkillSynchronizer>();
+      _hitRistrictor = new HitRistrictor(_hitInfo);
+
+      MonoUtility.Instance.StoppableDelaySec(_existTime, "NageSpaceFalse" + GetInstanceID().ToString(), () => {
+        gameObject.SetActive(false);
+
+        // NOTE: See SMB-DestroySkillSelf
+        MonoUtility.Instance.StoppableDelaySec(5.0f, "NageSpaceDestroy" + GetInstanceID().ToString(), () => {
+          Destroy(gameObject);
+        });
+      });
+    }
+
+    void OnTriggerStay2D(Collider2D collider) {
+      if (PhotonNetwork.isMasterClient) {
+        var target = collider.gameObject.GetComponent<IPhoton>();
+
+        if (target == null)
+          return;
+        if (TeamChecker.IsSameTeam(collider.gameObject, _skillUserObj))
+          return;
+        if (_hitRistrictor.ShouldRistrict(collider.gameObject))
+          return;
+
+        DamageCalculator.Calculate(_skillUserObj, _attackInfo);
+
+        _synchronizer.SyncAttack(_skillUserViewID, target.PhotonView.viewID, DamageCalculator.Damage, DamageCalculator.IsCritical, HitEffectType.Nage);
+      }
+    }
+
+    void OnDestroy() {
+      if (photonView.isMine && SkillReference.Instance != null)
+        SkillReference.Instance.Remove(this);
+    }
+
+    [SerializeField] private AttackInfo _attackInfo;
+    [SerializeField] private HitInfo _hitInfo;
+
+    private SkillSynchronizer _synchronizer;
+    private HitRistrictor _hitRistrictor;
+    private float _existTime = 10.0f;
+  }
+}
+
