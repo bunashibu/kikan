2522f852f8b916163dd658a6bd49f77985abeb96
siun <siuncyclone@gmail.com>
2019-04-25 20:45:08 +0900

    Add WarriorZ

---
+++ Assets/Scripts/Skill/Warrior/WarriorZ.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class WarriorZ : Skill {
+    void Awake() {
+      _synchronizer = GetComponent<SkillSynchronizer>();
+      _hitRistrictor = new HitRistrictor(_hitInfo);
+
+      MonoUtility.Instance.StoppableDelaySec(_existTime, "WarriorZFalse" + GetInstanceID().ToString(), () => {
+        if (gameObject == null)
+          return;
+
+        gameObject.SetActive(false);
+
+        // NOTE: See SMB-DestroySkillSelf
+        MonoUtility.Instance.StoppableDelaySec(5.0f, "WarriorZDestroy" + GetInstanceID().ToString(), () => {
+          Destroy(gameObject);
+        });
+      });
+    }
+
+    void Start() {
+      transform.parent = _skillUserObj.transform;
+    }
+
+    void OnTriggerStay2D(Collider2D collider) {
+      if (PhotonNetwork.isMasterClient) {
+        var target = collider.gameObject.GetComponent<IPhoton>();
+
+        if (target == null)
+          return;
+        if (TeamChecker.IsSameTeam(collider.gameObject, _skillUserObj))
+          return;
+        if (_hitRistrictor.ShouldRistrict(collider.gameObject))
+          return;
+
+        DamageCalculator.Calculate(_skillUserObj, _attackInfo);
+
+        _synchronizer.SyncAttack(_skillUserViewID, target.PhotonView.viewID, DamageCalculator.Damage, DamageCalculator.IsCritical, HitEffectType.Warrior);
+      }
+    }
+
+    [SerializeField] private AttackInfo _attackInfo;
+    [SerializeField] private HitInfo _hitInfo;
+
+    private SkillSynchronizer _synchronizer;
+    private HitRistrictor _hitRistrictor;
+    private float _existTime = 7.0f;
+  }
+}
+
