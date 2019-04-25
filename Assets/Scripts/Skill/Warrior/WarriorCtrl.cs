08cb8d152bd02f9e50ce585b7e0edb9eef8b2050
siun <siuncyclone@gmail.com>
2019-04-26 08:26:29 +0900

    Add WarriorCtrl Image

---
+++ Assets/Scripts/Skill/Warrior/WarriorCtrl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Bunashibu.Kikan {
+  public class WarriorCtrl : Skill {
+    void Start() {
+      transform.parent = _skillUserObj.transform;
+    }
+  }
+}
+
