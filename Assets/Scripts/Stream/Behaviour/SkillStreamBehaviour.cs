3ba819cbaee00c90e4ad0d9574f00e23a52f41a5
siun <siuncyclone@gmail.com>
2019-04-27 02:04:53 +0900

    Clean SkillStreamBehaviour

--- Assets/Scripts/Stream/Behaviour/SkillStreamBehaviour.cs
+++ Assets/Scripts/Stream/Behaviour/SkillStreamBehaviour.cs
@@ -11,74 +11,87 @@ namespace Bunashibu.Kikan {
 
     void Start() {
       SkillStream.OnAttacked
-        .Subscribe(entity => {
-          bool isAlreadyDead = entity.Target.Hp.Cur.Value == entity.Target.Hp.Min.Value;
-          if (isAlreadyDead)
-            return;
-
-          entity.Target.Hp.Subtract(entity.Damage);
-
-          if (entity.Target is IPhotonBehaviour) {
-            var targetPhoton = (IPhotonBehaviour)entity.Target;
-            int damageSkin = 0;
-
-            if (entity.Target is IDamageSkin)
-              damageSkin = ((IDamageSkin)entity.Attacker).DamageSkinId;
-
-            HitEffectPopupEnvironment.Instance.PopupHitEffect(entity.HitEffectType, targetPhoton);
-            NumberPopupEnvironment.Instance.PopupDamage(entity.Damage, entity.IsCritical, damageSkin, targetPhoton);
-
-            bool isDead = entity.Target.Hp.Cur.Value == entity.Target.Hp.Min.Value;
-
-            if (targetPhoton.PhotonView.isMine && isDead) {
-              _battleSynchronizer.SyncKill(entity.Attacker);
-              _battleSynchronizer.SyncDie(entity.Target);
-            }
-          }
-
-          if (entity.Target is Enemy) {
-            var enemy = (Enemy)entity.Target;
-            enemy.TargetChaseSystem.SetChaseTarget(entity.Attacker.transform);
-          }
-        })
+        .Subscribe(entity => OnAttacked(entity) )
         .AddTo(gameObject);
 
       SkillStream.OnDebuffed
-        .Subscribe(entity => {
-          entity.Target.Debuff.DurationEnable(entity.DebuffType, entity.Duration);
-        })
+        .Subscribe(entity => entity.Target.Debuff.DurationEnable(entity.DebuffType, entity.Duration) )
         .AddTo(gameObject);
 
       SkillStream.OnHealed
-        .Subscribe(entity => {
-          bool isAlreadyDead = entity.Target.Hp.Cur.Value == entity.Target.Hp.Min.Value;
-          if (isAlreadyDead)
-            return;
-
-          entity.Target.Hp.Add(entity.Quantity);
-
-          if (entity.Target is IPhotonBehaviour) {
-            var targetPhoton = (IPhotonBehaviour)entity.Target;
-            HitEffectPopupEnvironment.Instance.PopupHitEffect(HitEffectType.Heal, targetPhoton);
-            NumberPopupEnvironment.Instance.PopupHeal(entity.Quantity, targetPhoton);
-          }
-        })
+        .Subscribe(entity => OnHealed(entity) )
         .AddTo(gameObject);
 
       SkillStream.OnForced
-        .Subscribe(entity => {
-          if (entity.IsNewAdd)
-            entity.Target.Rigid.velocity = new Vector2(0, 0);
-
-          entity.Target.Rigid.AddForce(entity.Direction * entity.Force, ForceMode2D.Impulse);
-        })
+        .Subscribe(entity => OnForced(entity) )
         .AddTo(gameObject);
 
       SkillStream.OnStatusFixed
-        .Subscribe(entity => {
-          entity.Target.Status.SetFixAtk(entity.FixAtk);
-        })
+        .Subscribe(entity => entity.Target.Status.SetFixAtk(entity.FixAtk) )
         .AddTo(gameObject);
+    }
+
+    private void OnAttacked(AttackFlowEntity entity) {
+      bool isAlreadyDead = entity.Target.Hp.Cur.Value == entity.Target.Hp.Min.Value;
+      if (isAlreadyDead)
+        return;
+
+      ApplyDamage(entity.Target, entity.Damage);
+
+      if (entity.Target is IPhotonBehaviour) {
+        HitEffectPopupEnvironment.Instance.PopupHitEffect(entity.HitEffectType, (IPhotonBehaviour)entity.Target);
+        PopupNumber(entity.Attacker, (IPhotonBehaviour)entity.Target, entity.Damage, entity.IsCritical);
+
+        // Only died player client does.
+        SyncKillAndDeath(entity.Attacker, entity.Target);
+      }
+
+      if (entity.Target is Enemy) {
+        var enemy = (Enemy)entity.Target;
+        enemy.TargetChaseSystem.SetChaseTarget(entity.Attacker.transform);
+      }
+    }
+
+    private void ApplyDamage(IOnAttacked target, int damage) {
+      target.Hp.Subtract(damage);
+    }
+
+    private void PopupNumber(IAttacker attacker, IPhotonBehaviour targetPhoton, int damage, bool isCritical) {
+      int damageSkin = 0;
+
+      if (attacker is IDamageSkin)
+        damageSkin = ((IDamageSkin)attacker).DamageSkinId;
+
+      NumberPopupEnvironment.Instance.PopupDamage(damage, isCritical, damageSkin, targetPhoton);
+    }
+
+    private void SyncKillAndDeath(IAttacker attacker, IOnAttacked target) {
+      bool isDead = target.Hp.Cur.Value == target.Hp.Min.Value;
+
+      if ( ((IPhotonBehaviour)target).PhotonView.isMine && isDead ) {
+        _battleSynchronizer.SyncKill(attacker);
+        _battleSynchronizer.SyncDie(target);
+      }
+    }
+
+    private void OnHealed(HealFlowEntity entity) {
+      bool isAlreadyDead = entity.Target.Hp.Cur.Value == entity.Target.Hp.Min.Value;
+      if (isAlreadyDead)
+        return;
+
+      entity.Target.Hp.Add(entity.Quantity);
+
+      if (entity.Target is IPhotonBehaviour) {
+        HitEffectPopupEnvironment.Instance.PopupHitEffect(HitEffectType.Heal, (IPhotonBehaviour)entity.Target);
+        NumberPopupEnvironment.Instance.PopupHeal(entity.Quantity, (IPhotonBehaviour)entity.Target);
+      }
+    }
+
+    private void OnForced(ForceFlowEntity entity) {
+      if (entity.IsNewAdd)
+        entity.Target.Rigid.velocity = new Vector2(0, 0);
+
+      entity.Target.Rigid.AddForce(entity.Direction * entity.Force, ForceMode2D.Impulse);
     }
 
     private BattleSynchronizer _battleSynchronizer;
