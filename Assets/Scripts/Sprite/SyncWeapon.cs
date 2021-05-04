4f0880e230bbb886b134f12c6e63d0657ecfd127
siun <siuncyclone@gmail.com>
2021-05-04 12:58:12 +0900

    Fix weapon sync

--- Assets/Scripts/Sprite/SyncWeapon.cs
+++ Assets/Scripts/Sprite/SyncWeapon.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 namespace Bunashibu.Kikan {
   public class SyncWeapon : MonoBehaviour {
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
-      /*
       if (stream.isWriting) {
         stream.SendNext(_renderer.flipX);
+        /*
         stream.SendNext(_anim.GetBool("X"));
         stream.SendNext(_anim.GetBool("Shift"));
         stream.SendNext(_anim.GetBool("Z"));
+        */
       }
       else {
         _renderer.flipX = (bool)stream.ReceiveNext();
+        /*
         _anim.SetBool("X", (bool)stream.ReceiveNext());
         _anim.SetBool("Shift", (bool)stream.ReceiveNext());
         _anim.SetBool("Z", (bool)stream.ReceiveNext());
+        */
       }
-      */
     }
 
     [SerializeField] private SpriteRenderer _renderer;
