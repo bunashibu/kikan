8f0641941ae522f29360e700a2b8dc16eab60292
siun <siuncyclone@gmail.com>
2021-05-06 17:40:45 +0900

    Add HelenaCtrl image

---
+++ Assets/Scripts/Skill/Helena/HelenaCtrl.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UniRx;
+using UniRx.Triggers;
+
+namespace Bunashibu.Kikan {
+  [RequireComponent(typeof(SkillSynchronizer))]
+  public class HelenaCtrl : Skill {
+    void Awake() {
+      _synchronizer = GetComponent<SkillSynchronizer>();
+      _hitRistrictor = new HitRistrictor(_hitInfo);
+    }
+
+    void OnTriggerEnter2D(Collider2D collider) {
+      if (PhotonNetwork.isMasterClient) {
+        var target = collider.gameObject.GetComponent<IPhoton>();
+
+        if (target == null)
+          return;
+        if (TeamChecker.IsSameTeam(collider.gameObject, _skillUserObj))
+          return;
+        if (_hitRistrictor.ShouldRistrict(collider.gameObject))
+          return;
+
+        DamageCalculator.Calculate(_skillUserObj, _attackInfo);
+
+        _synchronizer.SyncAttack(_skillUserViewID, target.PhotonView.viewID, DamageCalculator.Damage, DamageCalculator.IsCritical, HitEffectType.Helena);
+        _synchronizer.SyncDebuff(target.PhotonView.viewID, DebuffType.Slow, _duration);
+      }
+    }
+
+    [SerializeField] private AttackInfo _attackInfo;
+    [SerializeField] private HitInfo _hitInfo;
+
+    private SkillSynchronizer _synchronizer;
+    private HitRistrictor _hitRistrictor;
+    private float _duration = 2.0f;
+  }
+}
