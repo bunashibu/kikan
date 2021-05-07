c52a91294383b3131057613d28c62b21d6eac0ce
siun <siuncyclone@gmail.com>
2021-05-07 13:46:26 +0900

    Revert ManjiSpace power to 1.3 and 1.6

--- Assets/Scripts/Skill/Manji/ManjiSpace.cs
+++ Assets/Scripts/Skill/Manji/ManjiSpace.cs
@@ -45,10 +45,10 @@ namespace Bunashibu.Kikan {
       var skillUser = _skillUserObj.GetComponent<Player>();
 
       float statusRatio = 1.2f;
-      float powerRatio = 1.5f;
+      float powerRatio = 1.3f;
       if (skillUser.Level.Cur.Value >= 11)
         statusRatio = 1.4f;
-        powerRatio = 2.0f;
+        powerRatio = 1.6f;
 
       _spaceFixSpd = new FixSpd(skillUser.Status.Spd * statusRatio, FixSpdType.Buff);
       skillUser.FixSpd.Add(_spaceFixSpd);
