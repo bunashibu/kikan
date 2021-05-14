39587aeb473c6f0866cc8114f1fa7cdc8efca269
siun <siuncyclone@gmail.com>
2021-05-14 12:46:43 +0900

    Change ManjiSpace time from 20sec to 18sec

--- Assets/Scripts/Skill/Manji/ManjiSpace.cs
+++ Assets/Scripts/Skill/Manji/ManjiSpace.cs
@@ -81,7 +81,7 @@ namespace Bunashibu.Kikan {
     private SpriteRenderer _renderer;
 
     private Action ResetStatus;
-    private float _buffTime = 20.0f;
+    private float _buffTime = 18.0f;
     private FixSpd _spaceFixSpd;
   }
 }
