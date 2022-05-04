94c8de5a45f5a010ce70e48c57ec40d96e3875c2
siun <siuncyclone@gmail.com>
2022-05-04 16:25:36 +0900

    v0.13.1

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -6,6 +6,6 @@ namespace Bunashibu.Kikan {
   public class GameData : SingletonMonoBehaviour<GameData> {
     public string GameVersion { get { return _gameVersion; } }
 
-    private readonly string _gameVersion = "v0.13.1dev";
+    private readonly string _gameVersion = "v0.13.1";
   }
 }
