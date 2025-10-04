9a637ed76b715c5ad0eda50bbb843439f54706c8
siun <siuncyclone@gmail.com>
2025-10-05 08:46:33 +0900

    Update Unity to 2021.3.45f2

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -6,6 +6,6 @@ namespace Bunashibu.Kikan {
   public class GameData : SingletonMonoBehaviour<GameData> {
     public string GameVersion { get { return _gameVersion; } }
 
-    private readonly string _gameVersion = "v0.13.2";
+    private readonly string _gameVersion = "v0.13.3";
   }
 }
